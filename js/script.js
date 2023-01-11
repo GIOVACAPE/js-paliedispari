a72054ee3306aaa501543b60c2fed702c932b30e
Giovanni Capelli <giovacape01@gmail.com>
2023-01-11 09:49:43 +0100

    add step e console.log

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ STEP:
+ 1) definire una funzione per verificare se una parola è palindorma
+ 2) chiedere all'utente di inserire una parola (tramite prompt o input)
+ 3(usare la funzione precedentemente definita per verificare se l'input dell'utente è una parola palindorma
+    3.a) se la parola è palindorma, stampare in consoloe "la parola NON è palindroma"
+    3.b) se la parola NON è palindroma, stampare in console "la parola NON è palindorma"
+ */
+
+function checkPalindorma() {
+    // verificare se la parola è palindorma
+}
+
+const parolaUtente = prompt ('Inserisci una parola');
+console.log('parolaUtente', parolaUtente);
+
+if (checkPalindorma()) {
+    console.log('La parola è palindroma vai tra');
+}
+
+else{
+    console.log('la parola aimè NON è palindorma');
+}
